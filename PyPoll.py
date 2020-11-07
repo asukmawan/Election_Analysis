165f0cbe70f22ade051aa983b29932fa79bf28ed
asukmawan <asukmawan85@gmail.com>
2020-11-07 12:31:42 -0500

    Move CSV file to Resources + open file script add

---
+++ PyPoll.py
@@ -0,0 +1,19 @@
+
+# What is the total number of votes cast
+# A complete list of candidates who received votes
+# Total number of votes each candidate won
+# What is the percentage of votes each candidate won
+# Who is the winner of the election
+
+
+# Assign a variable for the file to load and the path.
+file_to_load = "Resource/election_results.csv"
+
+# Open the election results and read the file.
+election_data = open(file_to_load, 'r')
+
+# To do: perform analysis.
+
+# Close the file.
+election_data.close()
+
