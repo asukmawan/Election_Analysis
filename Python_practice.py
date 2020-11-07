39cbd1fa3d2800b1bb7bfe4c32207bc3e438172e
asukmawan <asukmawan85@gmail.com>
2020-11-07 01:26:09 -0500

    Downloaded the dataset

---
+++ Python_practice.py
@@ -0,0 +1,27 @@
+print("Hello World")
+(5 + 2) * 3
+8 // 5 - 3
+8 + 22 * 2 - 4
+16 - 3 / 2 + 7 - 1
+3 ** 3 % 5
+5 + 9 * 3 / 2 - 4
+
+#Data Structure - List
+#declare list variable and enter items into that list
+counties = ["Arapahoe","Denver","Jefferson"]
+
+#declare an empty list with function: list()
+
+my_list = list()
+print(counties)
+
+# or my_list = [  ] can be used as well
+
+#Data Structure - Index List
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == "Denver":
+    print(counties[1])
+
+for county in counties:
+    print(county)
