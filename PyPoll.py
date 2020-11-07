58cc6a4ccc82870a96afa47b16b2d06345132bca
asukmawan <asukmawan85@gmail.com>
2020-11-07 17:22:49 -0500

    My first write to txt file using Python

--- PyPoll.py
+++ PyPoll.py
@@ -5,15 +5,25 @@
 # What is the percentage of votes each candidate won
 # Who is the winner of the election
 
+# Import dependencies
+import csv
+import os
 
 # Assign a variable for the file to load and the path.
-file_to_load = "Resource/election_results.csv"
+file_to_load = os.path.join("Resources","election_results.csv")
 
 # Open the election results and read the file.
-election_data = open(file_to_load, 'r')
+with open(file_to_load, "r") as election_data:
 
 # To do: perform analysis.
+    print(election_data)
 
-# Close the file.
-election_data.close()
-
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+# Write some data to the file.
+# Write three counties to the file.
+    txt_file.write("Counties in the Election")
+    txt_file.write("\n------------------------")
+    txt_file.write("\nArapahoe\nDenver\nJefferson")
