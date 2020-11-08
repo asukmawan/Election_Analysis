de3fc44d2160917205f47780369239ad7dcb15f9
asukmawan <asukmawan85@gmail.com>
2020-11-07 21:19:36 -0500

    Added vote percentage

--- PyPoll.py
+++ PyPoll.py
@@ -16,6 +16,14 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 total_votes = 0
 # Declare list for candidate names
 candidate_options = []
+# Declare dictionary to store the candidate votes
+candidate_votes = {}
+# Declare a variable that will hold the empty string for the winning candidate
+winning_candidate = ""
+# Declare a variable for the winning count equal to zero
+winning_count = 0
+# Declare a variable for the winning percentage equal to zero
+winning_percentage = 0
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 # Read the file object with the reader function.
@@ -31,8 +39,24 @@ with open(file_to_load) as election_data:
     # Add candidate name to the candidate list - only if its not already on the list
         if candidate_name not in candidate_options:  
             candidate_options.append(candidate_name)
+            # start of tracking the new candidate vote
+            candidate_votes[candidate_name] = 0
+        # add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
+#Determine the percentage of votes for each candidate by looping through the counts.
+#1. Iterate through the candidate list
+for candidate_name in candidate_votes:
+    #2. Retrieve vote count of a candidate
+    votes = candidate_votes[candidate_name]
+    #3. Calculate the percentage of votes
+    vote_percentage = float(votes)/float(total_votes) * 100
+    #4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name} received {vote_percentage:.1f}% of the vote.")
+# Determine the winner
+
 # Print the total votes
 print(total_votes)
 print(candidate_options)    
+print(candidate_votes)
 
 
